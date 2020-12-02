368d2f9be78d04d901ce956b5ca11007beae6a10
Katelynn Burns <katelynnburns655@gmail.com>
2020-12-02 14:52:50 -0500

    Update: MapScene making module for multiple levels

---
+++ src/MapInfo.js
@@ -0,0 +1,20 @@
+const TILE_SIZE = 32;
+
+export const tileMaps = [
+  'assets/backgrounds/levelOne.json',
+]
+
+export const padlockLocation = [
+  {
+    x: 4.5,
+    y: 7.6
+  }
+]
+
+export const keyLocations = [
+  [
+    { x: 9.5 * TILE_SIZE, y: 4.5 * TILE_SIZE },
+    { x: 18.5 * TILE_SIZE, y: 18.5 * TILE_SIZE },
+    { x: 4.5 * TILE_SIZE, y: 15.5 * TILE_SIZE },
+  ],
+]
