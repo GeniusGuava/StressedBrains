7115f64db83f6f0645f1b581c7ee488ad1988f3e
Guoying Zhong <ffamochong@gmail.com>
2020-12-09 01:11:12 -0500

    WIP: add routes dir, main routes & secure routes

---
+++ routes/main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/status', (req, res, next) => {
+  res.status(200);
+  res.json({ status: 'ok' });
+});
+
+router.post('/signup', (req, res, next) => {
+  res.status(200);
+  res.json({ status: 'ok' });
+});
+
+router.post('/login', (req, res, next) => {
+  res.status(200);
+  res.json({ status: 'ok' });
+});
+
+router.post('/logout', (req, res, next) => {
+  res.status(200);
+  res.json({ status: 'ok' });
+});
+
+router.post('/token', (req, res, next) => {
+  res.status(200);
+  res.json({ status: 'ok' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/main');
+const secureRoutes = require('./routes/secure');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/', secureRoutes);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ message: '404 - Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), function () {
